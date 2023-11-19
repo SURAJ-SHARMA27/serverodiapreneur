f9cf6e2010b058c9fe9dced2f80491afe6cd9a32
SURAJ <90304648+SURAJ-SHARMA27@users.noreply.github.com>
2023-11-19 12:14:18 +0530

    Changed package.json

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express=require('express');
 const dotenv=require('dotenv')
 const mongoose=require('mongoose');
 const app=express();
-dotenv.config({path:"./config.env"})
+dotenv.config()
 require("./db/conn");
 // const User=require("./model/userSchema");
 app.use(express.json());
 app.use(require('./router/auth'));
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT;
 
 
 app.get('/signup',(req,res)=>{
